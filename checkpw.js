2eb6557ca34b5ee79bb0e4ee2a6a31e37e33d46a
Mdr-Pranav <167682080+Mdr-Pranav@users.noreply.github.com>
2024-08-10 17:31:48 +0530

    Create checkpw.js

---
+++ checkpw.js
@@ -0,0 +1,30 @@
+function checkPassword() {
+    // Get the entered password
+    let enteredPassword = document.getElementById("password").value;
+    let enteredId = document.getElementById("iden").value;
+
+    // Replace 'your_password' with the correct password
+    var correctPassword = "pa";
+    var correctiden = "cs362";
+    var correctiden1="Cs362";
+    var correctiden2="CS362";
+    
+
+    if (enteredId===correctiden & enteredPassword === correctPassword ) {
+    //   if (enteredPassword === correctPassword) {}
+        return true;
+    } 
+    if (enteredId===correctiden1 & enteredPassword === correctPassword ) {
+    //   if (enteredPassword === correctPassword) {}
+        return true;
+    } 
+    if (enteredId===correctiden2 & enteredPassword === correctPassword ) {
+    //   if (enteredPassword === correctPassword) {}
+        return true;
+    } 
+else {
+        alert("Please try again.");
+        // Prevent form submission if the password is incorrect
+        return false;
+    }
+}
